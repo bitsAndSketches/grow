87dfbbce9d83f1bb6003369a7475779e5878c794
= <lsimon@greenflex.com>
2020-11-08 09:35:26 +0100

    basic spawn: only one magic number left

--- src/Player.gd
+++ src/Player.gd
@@ -6,15 +6,19 @@ const plant_scene_path = "res://src/plant.tscn"
 export var gravity = Vector2(0, 800)
 export var speed = 800
 export var spawn_offset = 32
+export var y_offset = 6 
 
 var velocity = Vector2.ZERO
 var plant_scene = null
-var last_plant_right = INF
-var last_plant_left = -INF
+var last_plant_right = -INF
+var last_plant_left = +INF
 var is_landed = false
+var half_sprite = 0
 
 func _ready() -> void:
 	plant_scene = preload(plant_scene_path)
+	var plant_ref = plant_scene.instance()
+	half_sprite = (plant_ref.get_rect().size).x / 2
 
 func get_input(initial_velocity: Vector2) -> Vector2:
 	var new_velocity = Vector2(
@@ -28,18 +32,18 @@ func check_for_spawn() -> void:
 		is_landed = true
 		spawn(position.x)
 	elif is_on_floor() and (position.x - spawn_offset) > last_plant_right:
-		spawn(last_plant_right + 32)
+		spawn(last_plant_right + half_sprite)
 		
 	elif is_on_floor() and (position.x + spawn_offset) < last_plant_left:
-		spawn(last_plant_left - 32)
+		spawn(last_plant_left - half_sprite)
 
 func spawn(x_pos: float):
-		var plant = plant_scene.instance()
-		plant.position.y = position.y + 6
+		var plant: Sprite = plant_scene.instance()
+		plant.position.y = position.y + y_offset
 		plant.position.x = x_pos
 		get_parent().add_child(plant)
-		last_plant_left = plant.position.x - 32
-		last_plant_right = plant.position.x + 32
+		last_plant_left = plant.position.x - half_sprite
+		last_plant_right = plant.position.x + half_sprite
 	
 func _physics_process(delta: float) -> void:
 	if is_on_floor():
